e949a30c6d3bbbcd670ea4aba08f10929b207aca
omaku cla-petra <omaku.petra@gmail.com>
2021-08-10 13:16:33 +0300

    edited the Readme and added setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+from setuptools import setup
+
+setup(name='calculator',
+version='0.1',
+description='Installation of Calculator Package',
+url='https://github.com/clappy203/Calculator',
+author='Cla-petra Omaku',
+author_email='omaku.petra@gmail.com',
+license='MIT',
+packages=['src'],
+zip_safe=False)
